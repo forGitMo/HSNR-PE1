7fd83b48546b7a3c623a22d31b19b0ac5e83bd0e
Moritz Amelang <moritz.amelang@app-logik.de>
2024-05-23 09:41:21 +0200

    calc distance of two vectors

--- vecAsStruct/main.c
+++ vecAsStruct/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <math.h>
+typedef struct { double x; double y; } vec2d_t;
+
+double distance(vec2d_t *a, vec2d_t *b){
+    double distance;
+    double var = ((b->x - a->x) * (b->x - a->x)) + ((b->y - a->y) * (b->y - a->y));
+
+
+    distance =  sqrt(var);
+
+    return distance;
+}
+
 
 int main() {
-    printf("Hello, World!\n");
+    vec2d_t a = { 2.0, 3.0 };
+    vec2d_t b = { 4.0, 6.0 };
+    printf("%0.1lf",distance(&a, &b));
     return 0;
 }
